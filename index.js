96728d999333540686979bed2faf474124b5cfeb
Kiruidyno <kirui472@gmail.com>
2023-03-19 00:21:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Write your solution here!
+const cats=["Milo", "Otis", "Garfield"];
+    console.log(cats);
+
+
+function destructivelyAppendCat (Ralph){
+    cats.push ("Ralph");
+    return cats;
+}
+
+ function destructivelyPrependCat (Bob){
+    cats.unshift('Bob');
+ }
+
+
+ function destructivelyRemoveLastCat(){
+    cats.pop();
+ }
+
+
+
+ function destructivelyRemoveFirstCat(){
+    cats.shift();
+ }
+ 
+
+function appendCat (){
+    return [...cats, "Broom"]
+
+ }
+
+function prependCat(){
+    return["Arnold", ...cats]
+}
+
+
+function removeLastCat() {
+    return cats.slice(0, -1);
+}
+
+function removeFirstCat(){
+    return cats.slice(1)
+}
+
--- test/indexTest.js
+++ test/indexTest.js
@@ -14,7 +14,7 @@ describe('index.js', function () {
 
     describe('destructivelyAppendCat(name)', function () {
       it('appends a cat to the end of the cats array', function () {
-        destructivelyAppendCat('Ralph');
+        destructivelyAppendCat('Ralph'); 
 
         expect(cats).to.have.ordered.members(["Milo", "Otis", "Garfield", "Ralph"]);
       });
